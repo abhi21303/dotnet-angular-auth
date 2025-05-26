1e9b95dd485662471d2fa4bf524972ee8af503f9
abhi21303 <abhipatel21303@gmail.com>
2025-05-26 22:24:22 +0530

    Added Test Api

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWebApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult GetHello()
+        {
+            return Ok(new { message = "Hello from API!" });
+        }
+    }
+}
--- obj/Debug/net9.0/DotNetWebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/DotNetWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a213fedd59e66b02859a5200273f93a9dc02281d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
